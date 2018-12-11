6b46356186bed0b156a0883b237dfc6f0f713aca
jsophil <zhouy2048@gmail.com>
2018-12-11 17:20:30 +0800

    register admin

--- articles/models.py
+++ articles/models.py
@@ -8,3 +8,6 @@ class Article(models.Model):
 
     created_date = models.DateTimeField(auto_now=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
