64ec83cd0db52f585090d73e55fa8830090e8ae6
jsophil <zhouy2048@gmail.com>
2018-12-11 17:34:39 +0800

    hh

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     body = models.TextField()
 
     created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField()
 
     def __str__(self):
         return self.title
