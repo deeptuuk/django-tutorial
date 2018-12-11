7f7f90f4b3bed5d520f11f8998abdf48f004e5f6
jsophil <zhouy2048@gmail.com>
2018-12-11 18:07:01 +0800

    hh

--- articles/views.py
+++ articles/views.py
@@ -3,5 +3,5 @@ from .models import Article
 
 # Create your views here.
 def article_list(request):
-    articles = Article.objects.all().order('created_date')
+    articles = Article.objects.all().order_by('-created_date')
     return render(request, 'articles/article_list.html', {'articles':articles})
