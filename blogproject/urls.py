cba5d657bad72c1fe9041b4191a95fe63b0e0fbe
jsophil <zhouy2048@gmail.com>
2018-12-12 16:48:28 +0800

    hh

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
 ]
+
+urlpatterns +=staticfiles_urlpatterns()
