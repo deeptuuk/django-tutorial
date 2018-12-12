0c6b2133b40cff36d0088b4a3da5cedacb31052d
jsophil <zhouy2048@gmail.com>
2018-12-12 23:13:39 +0800

    h

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
-    thumb = mdoels.ImageField(default='default.png', blank=True)
+    thumb = models.ImageField(default='default.png', blank=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField()
