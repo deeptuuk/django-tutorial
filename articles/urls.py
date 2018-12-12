bf2b534eb550762bd3c0f85468728bfa83ad2351
jsophil <zhouy2048@gmail.com>
2018-12-12 17:39:42 +0800

    hh

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.article_list),
+    path('', views.article_list),
+    path('<slug>/', view.article_detail),
 ]
