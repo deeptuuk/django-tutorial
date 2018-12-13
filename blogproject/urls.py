62c39d5be353d44fbec7c4280a6713132c858c23
jsophil <zhouy2048@gmail.com>
2018-12-13 14:29:55 +0800

    now

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/articles/')),
 ]
 
 urlpatterns +=staticfiles_urlpatterns()
