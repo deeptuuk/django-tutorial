1944d8e0a2122c2e1d771053dd2b15004c6746d7
jsophil <zhouy2048@gmail.com>
2018-12-13 09:02:22 +0800

    h

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns +=staticfiles_urlpatterns()
