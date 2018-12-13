c49b1ce58f3594c37c0210d5d98bc72eb1e71797
jsophil <zhouy2048@gmail.com>
2018-12-13 09:04:37 +0800

    h

--- accounts/views.py
+++ accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def signup_view(request):
-    render(request, 'accounts/signup.html')
+    return render(request, 'accounts/signup.html')
