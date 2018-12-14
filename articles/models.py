925c79a5976ea18774b246cb8345a06f92e257f1
jsophil <zhouy2048@gmail.com>
2018-12-14 15:30:35 +0800

    f

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class Archive(models.Model):
-    name = models.CharField(max_length=100,blank=True)
+    name = models.CharField(max_length=100,defaut=None)
 
     def __str__(self):
         return self.name
