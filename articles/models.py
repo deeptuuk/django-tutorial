5f06a7e44e57ef9b65cfd4eed140b501b5cec554
jsophil <zhouy2048@gmail.com>
2018-12-14 14:36:00 +0800

    h

--- articles/models.py
+++ articles/models.py
@@ -3,12 +3,9 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
     body = models.TextField()
-    thumb = models.ImageField(default='default.png', blank=True)
 
     created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField()
 
     def __str__(self):
         return self.title
