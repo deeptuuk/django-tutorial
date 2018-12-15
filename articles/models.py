f72c9471ebab2ada18b05eb758cb334e7d4ec868
jsophil <zhouy2048@gmail.com>
2018-12-15 11:15:53 +0800

    h

--- articles/models.py
+++ articles/models.py
@@ -7,26 +7,19 @@ from django.utils.html import strip_tags
 class Article(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    excerpt = models.CharField(max_length=200, blank=True)
     archive = models.ForeignKey('Archive',on_delete=models.PROTECT,null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        # 如果没有填写摘要
-        if not self.excerpt:
-            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
-            md = markdown.Markdown(extensions=[
-                'markdown.extensions.extra',
-                'markdown.extensions.codehilite',
-            ])
-            # 先将 Markdown 文本渲染成 HTML 文本
-            # strip_tags 去掉 HTML 文本的全部 HTML 标签
-            # 从文本摘取前 54 个字符赋给 excerpt
-            self.excerpt = strip_tags(md.convert(self.body))[:100]
-
-        # 调用父类的 save 方法将数据保存到数据库中
-        super(Article, self).save(*args, **kwargs)
-
+    def snippet(self):
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        # 先将 Markdown 文本渲染成 HTML 文本
+        # strip_tags 去掉 HTML 文本的全部 HTML 标签
+        # 从文本摘取前 54 个字符赋给 excerpt
+        tmp = strip_tags(md.convert(self.body))[:100]
+        return tmp
 
     def __str__(self):
         return self.title
