a1c1316daa49c2ed8699e7d2dcb49cf8c3d20f2f
jsophil <zhouy2048@gmail.com>
2018-12-15 11:01:02 +0800

    git

--- articles/models.py
+++ articles/models.py
@@ -1,19 +1,35 @@
 from django.db import models
 from django.urls import reverse
-
+import markdown
+from django.utils.html import strip_tags
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
+    excerpt = models.CharField(max_length=200, blank=True)
     archive = models.ForeignKey('Archive',on_delete=models.PROTECT,null=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
+
+
     def __str__(self):
         return self.title
-
-    def snippet(self):
-        return self.body[:50] + '...'
 
     def get_absolute_url(self):
         return reverse('articles:detail', kwargs={'pk': self.pk})
@@ -25,4 +41,4 @@ class Archive(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('articles:archive-detail', kwargs={'pk': self.pk})    
+        return reverse('articles:archive-detail', kwargs={'pk': self.pk})
